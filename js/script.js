9eac7c08d1f08e28b81a5f854b108a10a30cd616
hoanghongchuong <chuonghoanghong@gmail.com>
2018-11-20 17:11:45 +0700

    a

--- js/script.js
+++ js/script.js
@@ -69,6 +69,22 @@ $(document).ready(function($){
 		    // $('.logo').css('display','block');
 		  }
 		}
+
+	$('.slider-for').slick({
+	  	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	arrows: false,
+	  	fade: true,
+	  	asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+	  	slidesToShow: 3,
+	  	slidesToScroll: 1,
+	  	asNavFor: '.slider-for',
+	  	dots: true,
+	  	centerMode: true,
+	  	focusOnSelect: true
+	});
 });
 
 var MobileMenu = function () {
